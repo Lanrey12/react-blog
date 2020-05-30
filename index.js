d89b291d2eb9281ca888bdbc130e54dff2086fa2
Lanrey12 <alexolajide197@gmail.com>
2020-05-30 09:37:12 -0700

    moved private info into dev.js

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const config = require('./config/key')
+
+const { User } = require('./models/user')
+
 //connect mongodb usig mongoose
 const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://alex197:alex197@cluster0-xtbut.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect( config.mongoURI,
   {
     useNewUrlParser: true, useUnifiedTopology: true,
     useCreateIndex: true, useFindAndModify: false
 })
   .then(() => console.log('Db connected'))
   .catch(err => console.error(err));
-                           
+  
+//middleware for cookie parser setup
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }))
+//to get json data
+// support parsing of application/json type post data
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+
 //setup routes
-app.get('/', (req, res) => {
-    res.send('hello crusher blackwell')
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({ success: true })
+    })
 })
 
-
-
+// server port
 app.listen(5000)
 
